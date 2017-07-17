a0dd9d9d2a28b2272a95961c1d805efd140a954c
takoz53 <berkannur53@gmail.com>
2017-07-17 15:51:00 +0200

    get sets for player resources
    player individually now has resources

--- SK_Strategygame/SK_Strategygame/Domain/Player/Player.cs
+++ SK_Strategygame/SK_Strategygame/Domain/Player/Player.cs
@@ -12,10 +12,15 @@ namespace SK_Strategygame.Domain.Player
     class Player : Sprite
     {
         private String id;
+        private int availableMoney, availableWood, availableStone, availableFood;
         private Coordinate coordinate;
         private int x, y;
-        public Player(string path, Coordinate coordinate, String id) : base(path, coordinate.getX() * 250, coordinate.getY() * 250)
+        public Player(string path, Coordinate coordinate, String id, int startMoney, int startWood, int startStone, int startFood) : base(path, coordinate.getX() * 250, coordinate.getY() * 250)
         {
+            availableFood = startFood;
+            availableMoney = startMoney;
+            availableStone = startStone;
+            availableWood = startWood;
             this.coordinate = coordinate;
             this.id = id;
         }
@@ -38,6 +43,61 @@ namespace SK_Strategygame.Domain.Player
         public int setLocationY(float x)
         {
             return this.y;
+        }
+
+        public int getCurrentWood()
+        {
+            return availableWood;
+        }
+
+        public int getCurrentFood()
+        {
+            return availableFood;
+        }
+
+        public int getCurrentMoney()
+        {
+            return availableMoney;
+        }
+        public int getCurrentStone()
+        {
+            return availableStone;
+        }
+
+        public void increaseWoodAmount(int amount)
+        {
+            availableWood += amount;
+        }
+        public void decreaseWoodAmount(int amount)
+        {
+            availableWood -= amount;
+        }
+
+        public void increaseMoneyAmount(int amount)
+        {
+            availableMoney += amount;
+        }
+        public void decreaseMoneyAmount(int amount)
+        {
+            availableMoney -= amount;
+        }
+
+        public void increaseStoneAmount(int amount)
+        {
+            availableStone += amount;
+        }
+        public void decreaseStoneAmount(int amount)
+        {
+            availableStone -= amount;
+        }
+
+        public void increaseFoodAmount(int amount)
+        {
+            availableFood += amount;
+        }
+        public void decreaseFoodAmount(int amount)
+        {
+            availableFood -= amount;
         }
     }
 }
